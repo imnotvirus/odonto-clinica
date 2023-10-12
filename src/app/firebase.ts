ee05f9366a89fcb62c95a815b85009b4b4d06fb2
Luiz Claudio <lcluizclaudio96@gmail.com>
2023-10-11 21:59:28 -0300

    export firestore

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,6 +1,6 @@
 import {getApp, getApps, initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
-
+import {getFirestore} from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,5 +14,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth();
-
-export {app, auth};
+const firestore = getFirestore(app);
+export {app, auth, firestore};
